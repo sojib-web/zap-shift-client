914250637b25f5992a0c12e3cc2753cf178a3c42
Md Sojib Ali <alisojib295@gmail.com>
2025-07-02 02:04:50 +0600

    66-7 payment save hocce na kno database a

--- src/pages/Dashboard/CheckoutForm/CheckoutForm.jsx
+++ src/pages/Dashboard/CheckoutForm/CheckoutForm.jsx
@@ -81,6 +81,27 @@ const CheckoutForm = () => {
         if (result.paymentIntent.status === "succeeded") {
           console.log("Payment successful");
           console.log(result);
+          try {
+            const paymentDoc = {
+              email: user.email,
+              parcelId,
+              transactionId: result.paymentIntent.id,
+              amount,
+              paymentMethod: result.paymentIntent.payment_method_types,
+            };
+
+            const paymentRes = await axiosSecure.post("/payments", paymentDoc);
+            if (paymentRes.data.insertedId) {
+              console.log("✅ Payment added to database");
+            } else {
+              console.log("❌ Payment insert failed:", paymentRes.data);
+            }
+          } catch (err) {
+            console.error(
+              "❌ Error saving payment:",
+              err.response?.data || err.message
+            );
+          }
         }
       }
     }
